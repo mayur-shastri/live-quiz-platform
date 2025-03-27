c55f0b6721a75ba3b02b70397940454e3f46b682
Mau_2993 <shastrimayur2993@gmail.com>
2025-03-27 16:17:53 +0530

    Fixed axios config

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? 
-  'http://localhost:3000' : import.meta.env.BACKEND_URL,
+  baseURL: import.meta.env.MODE === 'development' ? 
+  'http://localhost:3000' : import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
 
